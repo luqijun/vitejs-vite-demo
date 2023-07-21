a5282896d3b8a55eecffb83039e08fd508ed756f
lqjun <1120948897@qq.com>
2023-07-21 10:46:02 +0800

    add solution to hot reload when using WSL2 on windows

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server:{
+    watch: {
+      // usePolling: true  // Windows使用WSL2必须开启才能热更新
+    }
+  }
 })
